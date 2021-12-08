018e84d4faff6dc5a4a80f9c4d49ad30b5cf7e19
srooland95 <srooland95@gmail.com>
2021-12-08 15:52:11 +0100

    set up basic flatlist

--- src/screens/Timer.js
+++ src/screens/Timer.js
@@ -1,84 +1,42 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import {
   View,
   Text,
   Dimensions,
   StyleSheet,
-  Animated,
-  TouchableOpacityBase,
   TouchableOpacity,
+  Animated,
+  FlatList,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get("screen");
 
 const colors = {
-  black: "#323F4E",
-  red: "#F76A6A",
-  white: "#ffffff",
+  black: "#292C31",
+  white: "#FDFDFD",
+  main: "#A9DDD8",
 };
-const ITEM_SIZE = width * 0.38;
-const ITEM_SPACING = (width - ITEM_SIZE) / 2;
+const ITEM_SIZE = width * 0.4;
+const ITEM_SPACING = (width - ITEM_SIZE) * 0.5;
 
 const timers = [...Array(13).keys()].map((i) => i * 5).slice(-12);
 
 const Timer = () => {
-  const scrollX = useRef(new Animated.Value(0)).current;
-  const [timerDuration, setTimerDuration] = useState(timers[0]);
-
   return (
     <View style={styles.container}>
       <StatusBar style={"light"} />
-      <Animated.View
-        style={[
-          StyleSheet.absoluteFillObject,
-          {
-            justifyContent: "flex-end",
-            alignItems: "center",
-            paddingBottom: 100,
-          },
-        ]}
-      >
-        <TouchableOpacity onPress={() => {}}>
-          <View style={styles.button}></View>
-        </TouchableOpacity>
-      </Animated.View>
-      <View style={styles.flatListContainer}>
-        <Text style={styles.number}>{timerDuration}</Text>
-        <Animated.FlatList
-          style={{ flexGrow: 0 }}
+      <View style={styles.timePickerContainer}>
+        <FlatList
           data={timers}
           keyExtractor={(item) => item.toString()}
+          style={{ flexGrow: 0 }}
           horizontal
-          bounces={false}
           showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: ITEM_SPACING }}
+          bounces={false}
           snapToInterval={ITEM_SIZE}
-          decelerationRate="fast"
-          onScroll={Animated.event(
-            [{ nativeEvent: { contentOffset: { x: scrollX } } }],
-            { useNativeDriver: true }
-          )}
-          onMomentumScrollEnd={(ev) => {
-            const index = Math.round(
-              ev.nativeEvent.contentOffset.x / ITEM_SIZE
-            );
-            setTimerDuration(timers[index]);
-          }}
+          contentContainerStyle={{ paddingHorizontal: ITEM_SPACING }}
           renderItem={({ item, index }) => {
-            const inputRange = [
-              (index - 1) * ITEM_SIZE,
-              index * ITEM_SIZE,
-              (index + 1) * ITEM_SIZE,
-            ];
-            const opacity = scrollX.interpolate({
-              inputRange,
-              outputRange: [0.4, 1, 0.4],
-            });
-            const scale = scrollX.interpolate({
-              inputRange,
-              outputRange: [0.7, 1, 0.7],
-            });
             return (
               <View
                 style={{
@@ -87,22 +45,22 @@ const Timer = () => {
                   alignItems: "center",
                 }}
               >
-                <Animated.Text
-                  style={[
-                    styles.number,
-                    {
-                      opacity,
-                      transform: [{ scale }],
-                    },
-                  ]}
-                >
-                  {item}
-                </Animated.Text>
+                <Text style={styles.number}>{item}</Text>
               </View>
             );
           }}
         />
       </View>
+      <TouchableOpacity style={styles.buttonContainer}>
+        <View
+          style={{
+            width: 70,
+            height: 70,
+            backgroundColor: colors.main,
+            borderRadius: 50,
+          }}
+        ></View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -110,25 +68,22 @@ const Timer = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: colors.black,
+  },
+  timePickerContainer: {
+    height: ITEM_SIZE,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    position: "absolute",
+    bottom: height * 0.1,
   },
   number: {
     color: colors.white,
     fontSize: ITEM_SIZE * 0.8,
-    fontWeight: "900",
-  },
-  flatListContainer: {
-    position: "absolute",
-    top: height * 0.3,
-    left: 0,
-    right: 0,
-    flex: 1,
-  },
-  button: {
-    width: 80,
-    height: 80,
-    borderRadius: 80,
-    backgroundColor: colors.red,
   },
 });
 
